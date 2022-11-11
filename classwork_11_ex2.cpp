34bf91b7ee87064521e344b0c33c736e9ad4a6e4
VitaliiReshetnyk <113349766+VitaliiReshetnyk@users.noreply.github.com>
2022-11-11 20:50:52 +0200

    Vitalii Reshetnyk hw&cw 11

---
+++ classwork_11_ex2.cpp
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+//10_2
+
+typedef struct
+{
+    char col;
+    int row;
+} Field;
+
+bool correctness(Field x)
+{
+    if (x.col < 'a' || x.col > 'h')
+        return false;
+
+    if (x.row < 1 || x.row > 8)
+        return false;
+
+    return true;
+}
+
+Field field_input()
+{
+    Field t;
+    printf("ColRow:");
+    scanf("%c%i", &t.col, &t.row);
+    scanf("%*c");//для уникнення передавання переводу стрічки
+    return t;
+}
+
+void output(const Field x)
+{
+    printf("%c:%u", x.col, x.row);
+}
+
+bool check(Field x, Field y)
+{
+    if (x.col == y.col) return true;
+    if (x.row == y.row) return true;
+    if ((x.col - y.col) == (x.row - y.row)) return true;
+    if ((x.col - y.col) == (y.row - x.row)) return true;
+
+    return false;
+}
+
+
+int main()
+{
+    Field pstn1, pstn2;
+    do
+    {
+        printf("Field1:\n");
+        pstn1 = field_input();
+    } while (!correctness(pstn1));
+    
+    do
+    {
+        printf("Field2:\n");
+        pstn2 = field_input();
+    } while (!correctness(pstn2));
+
+    if (check(pstn1, pstn2))
+    {
+        printf("\nYes, it can move from ");
+    }
+    else
+    {
+        printf("\nNo, it can't move from ");
+    }
+
+    output(pstn1);
+    printf(" to ");
+    output(pstn2);
+}
---
+++ classwork_11_ex3.cpp
@@ -0,0 +1,168 @@
+#include <stdio.h>
+//10_3
+
+typedef struct 
+{
+    int day;
+    int month;
+    int year;
+} Date;
+
+
+int Month[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+int checkDate(const Date* x)
+{
+    if (x->year<1980 || (*x).year>2200) return -1;
+    if ((*x).month<1 || x->month>12) return -2;
+    if (x->day <= 0 || (*x).day>Month[x->month-1]) return -3;
+
+    return 0;
+}
+
+int input(Date* x)
+{
+    scanf("%d", &x->day);
+    scanf("%d", &x->month);
+    scanf("%d", &x->year);
+    return 0;
+}
+
+void vyvod (const Date x)
+{
+    printf("\nDate= %2d.%2d.%4d", x.day, x.month, x.year);
+}
+
+int leapYear(int year)
+{
+    if ((year%400)==0)
+    {
+        return 1;
+    }
+    if ((year%100)==0)
+    {
+        return 0;
+    }
+    if ((year%4)==0)
+    {
+        return 1;
+    } 
+    return 0;
+}
+
+Date yesterday(const Date x)
+{
+    Date res = x;
+    if (x.day>1)
+    {
+        res.day--;
+        return res;
+    }
+
+    if (x.month>1)
+    {
+        res.month--;
+        res.day = Month[res.month-1];
+        if(leapYear(x.year) && res.month==2) res.day=29;
+        return res;
+    }
+
+    if (x.month==1)
+    {
+        res.year--;
+        res.month=12;
+        res.day = 31;
+        return res;
+    }
+    fprintf(stderr, "Yakas figna");
+    return res;
+}
+
+Date tomorrow(const Date x)
+{
+    Date res = x;
+    if(res.day<Month[x.month-1]+((x.month==2) ? leapYear(x.year) : 0))
+    {
+        res.day++;
+        return res;
+    }
+
+    if (x.month<12)
+    {
+        res.month++;
+        res.day = 1;
+        return res;
+    }
+
+    if (x.month==12)
+    {
+        res.year++;
+        res.month=1;
+        res.day = 1;
+        return res;
+    }
+    fprintf(stderr, "Yakas fignya");
+    return res;
+}
+
+int day_of_week (Date x)
+{
+    int ans = 3;
+    Date rep = {1, 1, 1981};
+    while (!((rep.day == x.day) && (rep.month == x.month) && (rep.year == x.year)))
+    {
+        ans++;
+        rep = tomorrow(rep);
+    }
+    ans %= 7;
+    //ans = 6 - ans;
+    printf("\n");
+    switch (ans)
+    {
+    case 0:
+        printf("Monday");
+        break;
+    case 1:
+        printf("Tuesday");
+        break;
+    case 2:
+        printf("Wednesday");
+        break;
+    case 3:
+        printf("Thursday");
+        break;
+    case 4:
+        printf("Friday");
+        break;
+    case 5:
+        printf("Saturday");
+        break;
+    case 6:
+        printf("Sunday");
+        break;        
+    default:
+        printf("something went wrong");
+        return -1;
+        break;
+    }
+    return 0;
+}
+
+
+int main()
+{
+    Date dat1;
+
+    do{
+        printf("input date:\n");
+        input(&dat1);
+        if(checkDate(&dat1)==0) break;
+    }while(1);
+
+    vyvod(dat1);
+    day_of_week(dat1);
+    printf("\nyesterday:");
+    vyvod(yesterday(dat1));
+    printf("\ntomorrow:");
+    vyvod(tomorrow(dat1));
+}
